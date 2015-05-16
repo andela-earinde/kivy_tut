4cdb306fd86ed277f59012d172121e5bf2c46e49
Arinde Eniola <eniola.arinde@andela.co>
2015-05-16 13:18:53 +0100

    Display the clock app and make it show the current time, implement also the widget for implementing the staop watch

--- clock_app/main.py
+++ clock_app/main.py
@@ -1,10 +1,27 @@
 from kivy.app import App
 from kivy.core.text import LabelBase 
+from time import strftime
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+class ClockLayout(BoxLayout):
+    time_prop = ObjectProperty(None)
 
 class ClockApp(App):
-    pass
+    
+    def update_time(self, nap):
+        self.root.time_prop.text = strftime('[b]%H[/b]:%M:%S')
+
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 1)
 
 if __name__ == "__main__":
+    from kivy.core.window import Window
+    from kivy.utils import get_color_from_hex
+
+    Window.clearcolor = get_color_from_hex('#101216')
+
     LabelBase.register(name="Roboto",
                        fn_regular="Roboto-Thin.ttf",
                        fn_bold='Roboto-Medium.ttf')
