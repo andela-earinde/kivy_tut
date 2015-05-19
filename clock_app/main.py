27a5e4364fddda13a4adcf3dd03c32623ed3d940
Arinde Eniola <eniola.arinde@andela.co>
2015-05-19 07:59:47 +0100

    complte the clock app, can start and stop the stop watch now

--- clock_app/main.py
+++ clock_app/main.py
@@ -9,12 +9,32 @@ class ClockLayout(BoxLayout):
     time_prop = ObjectProperty(None)
 
 class ClockApp(App):
-    
+    sw_started = False
+    sw_seconds = 0
+
     def update_time(self, nap):
+        if self.sw_started:
+            self.sw_seconds += nap
+        minutes, seconds = divmod(self.sw_seconds, 60)
         self.root.time_prop.text = strftime('[b]%H[/b]:%M:%S')
+        self.root.ids.stopwatch.text = (
+              '%02d:%02d.[size=40]%02d[/size]' %
+              (int(minutes), int(seconds),
+                int(seconds * 100 % 100))
+            )
 
     def on_start(self):
-        Clock.schedule_interval(self.update_time, 1)
+        Clock.schedule_interval(self.update_time, 0)
+
+    def start_stop(self):
+        self.root.ids.start_stop.text = ('Start' if self.sw_started else 'Stop')
+        self.sw_started = not self.sw_started
+
+    def reset(self):
+        if self.sw_started:
+            self.root.ids.start_stop.text = 'Start'
+            self.sw_started = False
+        self.sw_seconds = 0
 
 if __name__ == "__main__":
     from kivy.core.window import Window
